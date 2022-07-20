dc39225c3fbe909e782a128cd57345e7b8f96aec
Sewer <seweratwork@gmail.com>
2022-07-21 00:55:33 +0200

    Added: Function to Get Raw Buffer from ReusableMemStream

--- Source/Reloaded.Messaging/Messages/Disposables/ReusableSingletonMemoryStream.cs
+++ Source/Reloaded.Messaging/Messages/Disposables/ReusableSingletonMemoryStream.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+#if NET5_0_OR_GREATER
+using System.Runtime.CompilerServices;
+#endif
 using Microsoft.IO;
 using Reloaded.Messaging.Utilities;
 
@@ -15,6 +18,21 @@ public struct ReusableSingletonMemoryStream : IDisposable
     /// Provides access to the underlying span.
     /// </summary>
     public Span<byte> Span => SpanExtensions.AsSpanFast(Stream.GetBuffer(), (int)Stream.Length);
+
+    /// <summary>
+    /// Provides access to the underlying buffer. 
+    /// The length of buffer is denoted by the out value length.
+    /// </summary>
+    /// <param name="length">Length of the buffer.</param>
+#if NET5_0_OR_GREATER
+    [SkipLocalsInit]
+#endif
+    public byte[] GetBuffer(out int length)
+    {
+        var result = Stream.GetBuffer();
+        length = (int)Stream.Length;
+        return result;
+    }
 
     /// <summary/>
     /// <param name="stream">The stream in question.</param>
